4060b892343d310d5113a1830b5e18abb6c38d4a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-30 20:45:25 -0300

    beryllium: Fix faceunlock denial
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Change-Id: I87a58ee4cb28dc01748c473edb855d52ff6ac22e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,5 +41,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
