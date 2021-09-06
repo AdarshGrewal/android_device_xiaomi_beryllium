52b6caef7d1d6357d478eb5b66cc14140459b919
AdarshGrewal <adarshgrewal@gmail.com>
2021-09-06 23:14:42 +0530

    beryllium: libinit: Workaround safetynet
    Change-Id: I213feedbfe83c74cffc99c9e605cea9f8829f7aa

--- libinit/init_beryllium.cpp
+++ libinit/init_beryllium.cpp
@@ -52,4 +52,6 @@ void property_override(char const prop[], char const value[], bool add = true) {
 }
 
 void vendor_load_properties() {
+      property_override("ro.boot.verifiedbootstate", "green");
+      property_override("ro.boot.flash.locked", "1");
 }
